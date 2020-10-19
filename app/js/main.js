76b2826e7a3d41bb1c72e0721268d210f3b1d319
Andrey Sivak <riziy_bes@mail.ru>
2020-10-19 18:21:07 +0300

    change slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,26 @@
 
 window.addEventListener('load', function () {
 
-    $('.slider__wrap').owlCarousel({
-        loop: true,
-        items: 1,
-        mouseDrag: false,
-        touchDrag: false,
-        lazyLoad: true,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplaySpeed: 400,
-        dots: true,
-        autoplayHoverPause: true,
-        dotsContainer: $('.slider__dots')
-    });
+    (function homePageSlider() {
+        const slider = document.querySelector('.slider__wrap');
+
+        if( !slider ) {
+            return;
+        }
+
+        $( slider ).owlCarousel({
+            loop: true,
+            items: 1,
+            mouseDrag: false,
+            touchDrag: false,
+            lazyLoad: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplaySpeed: 400,
+            dots: true,
+            autoplayHoverPause: true,
+            dotsContainer: $('.slider__dots')
+        });
+    })();
 
 });
