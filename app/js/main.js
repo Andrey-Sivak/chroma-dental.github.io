4da01528f609f745a06f68063afba5ea56009456
Andrey Sivak <riziy_bes@mail.ru>
2020-10-27 09:57:40 +0300

    created mobile version for header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { Menu } from "./Menu";
+
 window.addEventListener('load', function () {
 
     (function homePageSlider() {
@@ -24,4 +26,14 @@ window.addEventListener('load', function () {
         });
     })();
 
+    (function menu() {
+        const menu = new Menu({
+            button: document.querySelector('.header-menu__btn'),
+            hamburger: document.querySelector('.header-menu__burger'),
+            menu: document.querySelector('.header__menu'),
+            activeClass: 'active'
+        });
+
+        menu.init();
+    })();
 });
