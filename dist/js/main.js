443853e55b8bd4f95f079369de9ecff5a89bd059
a.sivak <riziy_bes@mail.ru>
2020-10-29 01:40:04 +0300

    last changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -86,15 +86,27 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./app/js/Menu.js":
+/*!************************!*\
+  !*** ./app/js/Menu.js ***!
+  \************************/
+/*! exports provided: Menu */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Menu\", function() { return Menu; });\n\n\nclass Menu {\n  constructor(options) {\n    this.btn = options.button;\n    this.hamburger = options.hamburger;\n    this.activeClass = options.activeClass;\n    this.menu = options.menu;\n    this.menuWrap = this.menu.parentElement;\n    this.menuItems = this.menu.querySelector('.menu').children;\n    this.menuItems = Array.prototype.slice.call(this.menuItems);\n    this.toggleMenu = this.toggleMenu.bind(this);\n  }\n\n  toggleMenu() {\n    this.hamburger.classList.toggle(this.activeClass);\n    this.menuWrap.classList.toggle(this.activeClass);\n    this.menu.classList.toggle(this.activeClass);\n    this.menuItems.forEach(item => item.classList.toggle(this.activeClass));\n  }\n\n  listener() {\n    this.btn.addEventListener('click', this.toggleMenu);\n  }\n\n  init() {\n    if (!this.btn || !this.hamburger || !this.activeClass) {\n      return;\n    }\n\n    this.listener();\n  }\n\n}\n\n\n\n//# sourceURL=webpack:///./app/js/Menu.js?");
+
+/***/ }),
+
 /***/ "./app/js/main.js":
 /*!************************!*\
   !*** ./app/js/main.js ***!
   \************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("\n\nwindow.addEventListener('load', function () {\n  (function homePageSlider() {\n    const slider = document.querySelector('.slider__wrap');\n\n    if (!slider) {\n      return;\n    }\n\n    $(slider).owlCarousel({\n      loop: true,\n      items: 1,\n      mouseDrag: false,\n      touchDrag: false,\n      lazyLoad: true,\n      autoplay: true,\n      autoplayTimeout: 5000,\n      autoplaySpeed: 400,\n      dots: true,\n      autoplayHoverPause: true,\n      dotsContainer: $('.slider__dots')\n    });\n  })();\n});\n\n//# sourceURL=webpack:///./app/js/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Menu__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Menu */ \"./app/js/Menu.js\");\n\n\n\nwindow.addEventListener('load', function () {\n  (function homePageSlider() {\n    const slider = document.querySelector('.slider__wrap');\n\n    if (!slider) {\n      return;\n    }\n\n    $(slider).owlCarousel({\n      loop: true,\n      items: 1,\n      mouseDrag: false,\n      touchDrag: false,\n      lazyLoad: true,\n      autoplay: true,\n      autoplayTimeout: 5000,\n      autoplaySpeed: 400,\n      dots: true,\n      autoplayHoverPause: true,\n      dotsContainer: $('.slider__dots')\n    });\n  })();\n\n  (function menu() {\n    const menu = new _Menu__WEBPACK_IMPORTED_MODULE_0__[\"Menu\"]({\n      button: document.querySelector('.header-menu__btn'),\n      hamburger: document.querySelector('.header-menu__burger'),\n      menu: document.querySelector('.header__menu'),\n      activeClass: 'active'\n    });\n    menu.init();\n  })();\n});\n\n//# sourceURL=webpack:///./app/js/main.js?");
 
 /***/ })
 
